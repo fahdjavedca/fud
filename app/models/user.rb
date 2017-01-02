8222a6b3125138ffa8a004144ceadfa3e733c80a
Fahd Javed <fahdjavedca@gmail.com>
2017-01-02 18:18:22 -0500

    Added confirmable email address via Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+
   has_many :fud_entries
 end
 
