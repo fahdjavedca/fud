5673059bab60a167609476210e6c2dbc37167c26
Fahd Javed <fahdjavedca@gmail.com>
2017-01-02 18:56:13 -0500

    still trying to fix confirmation email

--- db/migrate/20170102231048_add_confirmable_to_devise.rb
+++ db/migrate/20170102231048_add_confirmable_to_devise.rb
@@ -9,8 +9,9 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
     # users as confirmed, do the following
-    Rails.env.production? execute("UPDATE users SET confirmed_at = NOW()")
-    Rails.env.development? execute("UPDATE users SET confirmed_at = date('now')")
+    #Rails.env.production? execute("UPDATE users SET confirmed_at = NOW()")
+    #Rails.env.development? execute("UPDATE users SET confirmed_at = date('now')")
+    User.update_all(:confirmed_at => Time.now)
     # All existing user accounts should be able to log in after this.
     # Remind: Rails using SQLite as default. And SQLite has no such function :NOW.
     # Use :date('now') instead of :NOW when using SQLite.
